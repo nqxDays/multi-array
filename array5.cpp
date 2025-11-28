96ce0b39297f1e8fc80436c5f159e956c6988a74
nqxDays <122733777+nqxDays@users.noreply.github.com>
2025-11-28 21:22:41 +0300

    Add files via upload

---
+++ array5.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    srand(time(0));
+
+    int rows, cols;
+    cout << "Rows: ";
+    cin >> rows;
+    cout << "Cols: ";
+    cin >> cols;
+
+
+    int** a = new int* [rows];
+    for (int i = 0; i < rows; i++)
+        a[i] = new int[cols];
+
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            a[i][j] = rand() % 11 - 5;
+
+
+    cout << "\nInitial array:\n";
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < cols; j++)
+            cout << a[i][j] << " ";
+        cout << endl;
+    }
+
+    int newRows = 0;
+    for (int i = 0; i < rows; i++) 
+    {
+        bool zeroFound = false;
+        for (int j = 0; j < cols; j++)
+            if (a[i][j] == 0) zeroFound = true;
+
+        if (!zeroFound) newRows++;
+    }
+
+
+    int** b = new int* [newRows];
+    int idx = 0;
+
+    for (int i = 0; i < rows; i++) 
+    {
+        bool zeroFound = false;
+        for (int j = 0; j < cols; j++)
+            if (a[i][j] == 0) zeroFound = true;
+
+        if (!zeroFound) 
+        {
+            b[idx] = new int[cols];
+            for (int j = 0; j < cols; j++)
+                b[idx][j] = a[i][j];
+            idx++;
+        }
+    }
+
+
+    cout << "\nArray not rows in zero:\n";
+    for (int i = 0; i < newRows; i++) {
+        for (int j = 0; j < cols; j++)
+            cout << b[i][j] << " ";
+        cout << endl;
+    }
+
+
+    for (int i = 0; i < rows; i++) 
+        delete[] a[i];
+    delete[] a;
+
+    for (int i = 0; i < newRows; i++) 
+        delete[] b[i];
+    delete[] b;
+
+    return 0;
+}
---
+++ array6.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    srand(time(0));
+
+    int rows, cols;
+    cout << "Rows: ";
+    cin >> rows;
+    cout << "Cols: ";
+    cin >> cols;
+
+    int** A = new int* [rows];
+    int** B = new int* [rows];
+    int** C = new int* [rows];
+
+    for (int i = 0; i < rows; i++) 
+    {
+        A[i] = new int[cols];
+        B[i] = new int[cols];
+        C[i] = new int[cols];
+    }
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++) 
+        {
+            A[i][j] = rand() % 21 - 10;
+            B[i][j] = rand() % 21 - 10;
+        }
+
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            C[i][j] = A[i][j] + B[i][j];
+
+
+    cout << "\nArray A:\n";
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < cols; j++)
+            cout << A[i][j] << " ";
+        cout << endl;
+    }
+
+    cout << "\nArray B:\n";
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < cols; j++)
+            cout << B[i][j] << " ";
+        cout << endl;
+    }
+
+    cout << "\nSum:\n";
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < cols; j++)
+            cout << C[i][j] << " ";
+        cout << endl;
+    }
+
+    for (int i = 0; i < rows; i++) 
+    {
+        delete[] A[i];
+        delete[] B[i];
+        delete[] C[i];
+    }
+
+    delete[] A;
+    delete[] B;
+    delete[] C;
+
+    return 0;
+}
